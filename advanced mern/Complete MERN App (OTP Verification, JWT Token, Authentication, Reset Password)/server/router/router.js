e0e65d4d3afbbd43e26f626af349d41c138049e1
jarray-kais <jarraykais1@gmail.com>
2024-01-25 19:02:45 +0100

    milgen
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- advanced mern/Complete MERN App (OTP Verification, JWT Token, Authentication, Reset Password)/server/router/router.js
+++ advanced mern/Complete MERN App (OTP Verification, JWT Token, Authentication, Reset Password)/server/router/router.js
@@ -5,12 +5,13 @@ const router = Router();
 
 /**import all controller */
 import * as controller from '../controllers/appController.js'
-
+import { registerMail } from "../controllers/mailer.js";
 import Auth ,{localVaribles} from "../middleware/Auth.js"
+
 
 /** POST Methods */
 router.route('/register').post(controller.register); // register user
-router.route('/registerMail').post(); // send the email
+router.route('/registerMail').post(registerMail); // send the email
 router.route('/authenticate').post(controller.verifyUser,(req,res)=>res.end); // authenticate user
 router.route('/login').post(controller.verifyUser,controller.login); // login in app
 
